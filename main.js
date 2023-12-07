f0ac4fe6054ca81ba879273dfbe6b32e6739b360
bingranyu <49502964+bingranyu@users.noreply.github.com>
2023-12-07 23:51:13 +0800

    update

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-$(window).resize(function(){let e=$("#content");clearTimeout(debounce),setTimeout(function(){e.height(0).height(window.innerHeight)},10)}).resize();var debounce,page_mods=[{name:"bopomofo_write",title:"ㄅㄆㄇ書寫學習"},{name:"exercise_book",title:"國字練習簿"}],main_sidebar_bs=new bootstrap.Offcanvas(".main-sidebar");page_mods.forEach(function(e){$(".main-sidebar .offcanvas-body ul").append(`<li class="nav-item"><a class="nav-link" target_page="${e.name}" href="#">${e.title}</a></li>`)}),$(".main-sidebar .offcanvas-body ul").on("click","a",function(e){$("[mod_type=page_mod]").remove(),import("./modules/pages/"+e.target.getAttribute("target_page")+".js").then(function(e){$("#content").html(e.content),$("head").append(e.style),null!==e.on_complete&&e.on_complete()}),main_sidebar_bs.hide()});
+// #content 自動調整fit畫面
+var debounce;
+$(window).resize(function() {
+  const el = $('#content');
+  clearTimeout(debounce);
+  setTimeout(function() {
+	el.height(0)
+	  .height(window.innerHeight);
+  }, 10);      
+}).resize();
+
+var page_mods = [{"name":'bopomofo_write',"title":"ㄅㄆㄇ書寫學習"},{"name":'exercise_book',"title":"國字練習簿"}];
+var main_sidebar_bs = new bootstrap.Offcanvas('.main-sidebar');
+page_mods.forEach(function(mod){
+	$(".main-sidebar .offcanvas-body ul").append(`<li class="nav-item"><a class="nav-link" target_page="${mod['name']}" href="#">${mod['title']}</a></li>`);
+});
+$(".main-sidebar .offcanvas-body ul").on("click","a",function(e){
+	$("[mod_type=page_mod]").remove();
+	import('./modules/pages/'+e.target.getAttribute("target_page")+'.js').then(function(mod){
+	   $("#content").html(mod.content);
+	   $("head").append(mod.style);
+	   if(mod.on_complete!==null){
+	     mod.on_complete();
+	   }
+	})
+	main_sidebar_bs.hide();
+});
+
+
--- modules/utility/utility.js
+++ modules/utility/utility.js
@@ -1,10 +1,5 @@
-export{multiplyMatrices,matrixTranspose2D,RemToPixels,getScroll,bopomofo_template,on_end_method,zip};function multiplyMatrices(m1,m2){var result=[];for(var i=0;i<m1.length;i++){result[i]=[];for(var j=0;j<m2[0].length;j++){var sum=0;for(var k=0;k<m1[0].length;k++){sum+=m1[i][k]*m2[k][j];}
-result[i][j]=sum;}}
-return result;}
-function matrixTranspose2D(m){return m[0].map((_,colIndex)=>m.map(row=>row[colIndex]));}
-function RemToPixels(rem){return rem*parseFloat(getComputedStyle(document.documentElement).fontSize);}
-var getScroll=function(){if(window.pageYOffset!=undefined){return[pageXOffset,pageYOffset];}else{var sx,sy,d=document,r=d.documentElement,b=d.body;sx=r.scrollLeft||b.scrollLeft||0;sy=r.scrollTop||b.scrollTop||0;return[sx,sy];}}
-var bopomofo_template=[(s)=>`<div style="height: ${s}px;width: ${s*0.5}px;position: relative;">
+export{multiplyMatrices,matrixTranspose2D,RemToPixels,getScroll,bopomofo_template,on_end_method,zip,tonal_marks,bopomo_category};function multiplyMatrices(m1,m2){var result=[];for(var i=0;i<m1.length;i++){result[i]=[];for(var j=0;j<m2[0].length;j++){var sum=0;for(var k=0;k<m1[0].length;k++){sum+=m1[i][k]*m2[k][j];}
+result[i][j]=sum;};};return result;};function matrixTranspose2D(m){return m[0].map((_,colIndex)=>m.map(row=>row[colIndex]));};function RemToPixels(rem){return rem*parseFloat(getComputedStyle(document.documentElement).fontSize);};var getScroll=function(){if(window.pageYOffset!=undefined){return[pageXOffset,pageYOffset];}else{var sx,sy,d=document,r=d.documentElement,b=d.body;sx=r.scrollLeft||b.scrollLeft||0;sy=r.scrollTop||b.scrollTop||0;return[sx,sy];};};var bopomofo_template=[(s)=>`<div style="height: ${s}px;width: ${s*0.5}px;position: relative;">
     <div class="tone_2" style="width: ${s*0.1}px;height: ${s*0.1}px;position: absolute;top: ${s*0.25}px;left: ${s*0.1}px;"></div>
 	<div class="bopomo_symbol1" style="width: ${s*0.3}px;height: ${s*0.3}px;position: absolute;top: ${s*0.35}px;left: 0px;"></div>
 	<div class="tone_1" style="width: ${s*0.2}px;height: ${s*0.2}px;position: absolute;top: ${s*0.25}px;left: ${s*0.3}px;"></div>
@@ -20,8 +15,4 @@ var bopomofo_template=[(s)=>`<div style="height: ${s}px;width: ${s*0.5}px;positi
 	<div class="bopomo_symbol3" style="width: ${s*0.3}px;height: ${s*0.3}px;position: absolute;top: ${s*0.7}px;left: 0px;"></div>
 	<div class="tone_1" style="width: ${s*0.2}px;height: ${s*0.2}px;position: absolute;top: ${s*0.55}px;left: ${s*0.3}px;"></div>
 </div>`];var play_list_audio=function(audio_list){var wordlist=JSON.parse(JSON.stringify(audio_list));var loop_play_audio=function(){var fpath=wordlist.shift()
-var audio=new Audio(fpath);if(wordlist.length>0){audio.addEventListener("ended",function(){loop_play_audio();});}
-audio.play();}
-loop_play_audio();}
-var on_end_method=function(){var onArgs=Array.prototype.slice.call(arguments);var originalFunction=this.__proto__[onArgs[0]];var givenCallback=onArgs[1];var callbackArgs=onArgs.slice(2);this.__proto__[onArgs[0]]=function(){var args=Array.prototype.slice.call(arguments);originalFunction.apply(this,args);givenCallback.apply(this,callbackArgs);}}
-var zip=rows=>rows[0].map((_,c)=>rows.map(row=>row[c]));
+var audio=new Audio(fpath);if(wordlist.length>0){audio.addEventListener("ended",function(){loop_play_audio();});};audio.play();};loop_play_audio();};var on_end_method=function(){var onArgs=Array.prototype.slice.call(arguments);var originalFunction=this.__proto__[onArgs[0]];var givenCallback=onArgs[1];var callbackArgs=onArgs.slice(2);this.__proto__[onArgs[0]]=function(){var args=Array.prototype.slice.call(arguments);originalFunction.apply(this,args);givenCallback.apply(this,callbackArgs);};};var zip=rows=>rows[0].map((_,c)=>rows.map(row=>row[c]));var tonal_marks=["ˊ","ˇ","ˋ","˙"];var bopomo_category={};"ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙ".split("").forEach((v)=>bopomo_category[v]=0);"ㄧㄨㄩ".split("").forEach((v)=>bopomo_category[v]=1);"ㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ".split("").forEach((v)=>bopomo_category[v]=2);
